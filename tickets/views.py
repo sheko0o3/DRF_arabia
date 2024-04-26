0376ba957ae062a465deee3ac93d4b2747621423
sherif <sherifshams1992@gmail.com>
2024-04-26 13:12:22 +0200

    req??

--- tickets/views.py
+++ tickets/views.py
@@ -203,6 +203,51 @@ class generics_pk(generics.RetrieveUpdateDestroyAPIView):
     # authentication_classes = [TokenAuthentication]
     # permission_classes = [IsAuthenticated]
 
+# # ViewSet ............................... #
+# class HelloViewSet(viewsets.ViewSet):
+#     """Test API ViewSet"""
+#     serializer_class = HelloSerializer
+#
+#     def list(self, request):
+#         """Return a hello message."""
+#         ...
+#
+#     def create(self, request):
+#         """Create a new hello message."""
+#         serializer = self.serializer_class(data=request.data)
+#
+#         if serializer.is_valid():
+#             name = serializer.validated_data.get('name')
+#             message = f'Hello {name}!'
+#             return Response({'message': message})
+#         else:
+#             return Response(
+#                 serializer.errors,
+#                 status=status.HTTP_400_BAD_REQUEST
+#             )
+#
+#     def retrieve(self, request, pk=None):
+#         """Handle getting an object by its ID"""
+#
+#         return Response({'http_method': 'GET'})
+#
+#     def update(self, request, pk=None):
+#         """Handle updating an object"""
+#
+#         return Response({'http_method': 'PUT'})
+#
+#     def partial_update(self, request, pk=None):
+#         """Handle updating part of an object"""
+#
+#         return Response({'http_method': 'PATCH'})
+#
+#     def destroy(self, request, pk=None):
+#         """Handle removing an object"""
+#
+#         return Response({'http_method': 'DELETE'})
+#
+
+
 
 # 7 ViewSets
 class viewsets_guest(viewsets.ModelViewSet):
